1f1fef8e9fc4ed8a51e34b1563fabef502c488f5
Rohan Meshram <117159898+RohanRPM@users.noreply.github.com>
2024-09-19 18:33:30 +0530

    Changed name to Parikshak

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -49,7 +49,7 @@ import { useNavigate } from 'react-router-dom';
 //   return (
 //     <div className="min-h-screen flex items-center justify-center bg-gray-100">
 //       <div className="bg-white p-8 rounded-lg shadow-md w-96">
-//         <h2 className="text-2xl font-bold mb-6 text-center">VulnAlert Login</h2>
+//         <h2 className="text-2xl font-bold mb-6 text-center">Parikshak Login</h2>
 
 //         {error && <div className="text-red-500 mb-4">{error}</div>}
 
@@ -103,7 +103,7 @@ const LoginPage = ({ onLogin }) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">VulnAlert Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Parikshak Login</h2>
         {!showMFA ? (
           <form
             onSubmit={(e) => {
